ec0e7d0c06dadbaaf63f2e5b2ac47471c5e0ac02
jtschindler <jtschindler@email.arizona.edu>
2022-06-02 10:45:55 +0200

    test

---
+++ sculptor_extensions/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import pkg_resources
+
+# Set the global version variable from the setup.py file
+try:
+    __version__ = pkg_resources.get_distribution('sculptor_extensions').version
+except Exception:
+    __version__ = 'unknown'
