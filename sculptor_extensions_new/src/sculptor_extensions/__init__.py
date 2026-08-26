053f3bd797b609e9418fc077dc4c68109f5ca720
Francesco Guarneri <50723359+G-Francio@users.noreply.github.com>
2026-08-26 08:47:33 +0200

    More migration to uv

--- sculptor_extensions_new/src/sculptor_extensions/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import pkg_resources
-
-# Set the global version variable from the setup.py file
-try:
-    __version__ = pkg_resources.get_distribution('sculptor_extensions').version
-except Exception:
-    __version__ = 'unknown'
